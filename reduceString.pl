a72551f440dca353568331c5b1fc72aeae9f019c
reductist <pvonrickenbach@gmail.com>
2020-01-11 22:16:22 -0500

    add note regarding attempts to resolve error

--- reduceString.pl
+++ reduceString.pl
@@ -17,7 +17,7 @@ sub reduce{
 	my $myAcc = undef;
 	foreach my $hash_ref (@str_list) {
 		foreach ($hash_ref->{text}) {
-			if ($myAcc) {
+			if (defined $myAcc) {
 				print $myAcc = $red->( $myAcc, $_ . ' ');
 			}
 			else {
@@ -34,3 +34,13 @@ sub myReducer{
 
 my $answer = reduce(@string_list, \&myReducer);
 print Dumper $answer;
+#╭─pvr@thoth ~/perl_scripts  ‹master›
+#╰─➤  ./reduceString.pl
+#Not a HASH reference at ./reduceString.pl line 19.
+#hello world from santa %
+
+# ... I have a feeling the issue is that I'm not assigning $myAcc in the else{} branch, but I've tried:
+# - switching the if/else
+# - moving the if (!defined $myAcc) { ... } between the two foreach's
+# - various alternative dereferencing syntaxes
+# ... but only made things worse. Not sure how to eliminate the error and the '%' at the end of $answer
